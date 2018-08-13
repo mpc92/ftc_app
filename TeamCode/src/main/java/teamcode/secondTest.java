5cc0f9f8f6b93b931a6854c79c3416a11c805c65
Yessica-C <tryben4242@gmail.com>
2018-08-13 10:52:14 -0700

    Drive system 2.2
    Second attempt at a drive system that can drive and turn at the same time, as well as changing some config so i can build to the 11970 phones

--- TeamCode/src/main/java/teamcode/secondTest.java
+++ TeamCode/src/main/java/teamcode/secondTest.java
@@ -17,8 +17,8 @@ public class secondTest extends LinearOpMode {
     // declaring hardware
     // variable type, Hardware type, variable name
     private ElapsedTime runtime = new ElapsedTime();
-    private DcMotor motor0;
-    private DcMotor motor1;
+    private DcMotor leftMotor;
+    private DcMotor rightMotor;
     private Servo servo0;
 
     @Override
@@ -31,22 +31,20 @@ public class secondTest extends LinearOpMode {
         runtime.reset();
 
         while(opModeIsActive()){
-
             //double is a variable type that supports decimals
             double leftPower;
             double rightPower;
-            double drive = -gamepad1.left_stick_y;
-            double turn = gamepad1.right_stick_x;
+            double leftStick = gamepad1.left_stick_y;
+            double rightStick = gamepad1.right_stick_x;
+            //defining a variable is just stating the type, and giving it a value, if applicable
 
-            if (drive !=0){
-                leftPower = (-drive/2);
-                rightPower = (-drive/2);
-                //if drive is not 0, set both motor powers to the value of drive
+            if (rightStick < 0) {
+                leftPower = leftStick + (2 * rightStick);
+                rightPower = leftStick;
             }
-            else if (turn !=0){
-                leftPower = (drive - (turn/2));
-                rightPower = (drive + (turn/2));
-                //if the drive if reports false, it runs this turn block
+            else if (rightStick > 0) {
+                leftPower = leftStick;
+                rightPower = leftStick - ( 2 * rightStick);
             }
             else {
                 leftPower = 0;
@@ -54,8 +52,8 @@ public class secondTest extends LinearOpMode {
                 //if neither of these if statements report true it sets the motor powers to 0
                 // if you don't push a stick, the robot don't move
             }
-            this.motor0.setPower(leftPower);
-            this.motor1.setPower(rightPower);
+            this.leftMotor.setPower(leftPower);
+            this.rightMotor.setPower(rightPower);
 
         }
 
@@ -66,13 +64,13 @@ public class secondTest extends LinearOpMode {
 
         //giving internal hardware an external name for the app config
         //also initializing the hardware?
-        this.motor0 = hardwareMap.get (DcMotor.class,"Left Motor");
-        this.motor1 = hardwareMap.get (DcMotor.class, "Right Motor");
+        this.leftMotor = hardwareMap.get (DcMotor.class,"Left Motor");
+        this.rightMotor = hardwareMap.get (DcMotor.class, "Right Motor");
         this.servo0 = hardwareMap.get (Servo.class, "Arm Servo");
 
         // makes the motors that face away from each other move the same direction
-        this.motor0.setDirection(DcMotor.Direction.FORWARD);
-        this.motor1.setDirection(DcMotor.Direction.REVERSE);
+        this.leftMotor.setDirection(DcMotor.Direction.FORWARD);
+        this.rightMotor.setDirection(DcMotor.Direction.REVERSE);
 
         telemetry.addData("Status", "Online");
         telemetry.update();

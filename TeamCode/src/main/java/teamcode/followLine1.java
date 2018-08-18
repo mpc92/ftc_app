4013033123257e6318727bec8fc3830a76207e8d
Yessica-C <tryben4242@gmail.com>
2018-08-17 17:51:19 -0700

    attempt 1 at a line following system

--- TeamCode/src/main/java/teamcode/followLine1.java
+++ TeamCode/src/main/java/teamcode/followLine1.java
@@ -34,22 +34,38 @@ public class followLine1 extends LinearOpMode {
         waitForStart();
         runtime.reset();
 
-        double armPostion = 0;
-        double leftPower = 0;
-        double rightPower = 0;
+        double leftPower;
+        double rightPower;
         double leftStickY;
         double rightStickX;
+        double armPos = 0;
+        boolean armGoingRight = true;
+        boolean followingLine = false;
 
-        this.armServo.setPosition(-.125);
+
+        this.armServo.setPosition(armPos);
 
 
        while (opModeIsActive()){
 
-            armPostion = this.armServo.getPosition();
-            leftStickY = -gamepad1.left_stick_y;
-            rightStickX = gamepad1.right_stick_x;
-            leftPower = 0;
-            rightPower = 0;
+
+           if (gamepad1.a && !followingLine) { //if line following is off, turns it on when a is pressed
+
+               followingLine = true;
+
+           }
+
+           if (gamepad1.a && followingLine) { // if line following is on, turns if off when a is pressed
+
+               followingLine = false;
+
+           }
+
+
+                leftStickY = -gamepad1.left_stick_y;
+                rightStickX = gamepad1.right_stick_x;
+                leftPower = 0;
+                rightPower = 0;
 
                if (leftStickY != 0) { //if driving
 
@@ -80,18 +96,84 @@ public class followLine1 extends LinearOpMode {
 
                }
 
-               else { // if the user does not have input, the robot will attempt to follow a line
 
-               }
 
-               this.leftMotor.setPower(leftPower);
-               this.rightMotor.setPower(rightPower);
+                   else if (followingLine) { //if the driver is not controlling, and line follow is on
 
-               telemetry.addData("Left Power", leftPower);
-               telemetry.addData("Right Power", rightPower);
 
-               telemetry.update();
 
-        }
+                        while (followingLine) { //repeat until line follow is disabled
+
+                            armPos = armServo.getPosition();
+
+                            if (armServo.getPosition() == 1) { // if the arm is all the way right, go left
+
+                                armGoingRight = false;
+
+                            }
+
+                            if (armServo.getPosition() == 0) { // if the arm is all the way left, go right
+
+                                armGoingRight = true;
+
+                            }
+
+                            if (armGoingRight) { //makes arm go right
+
+                                armServo.setPosition(armPos + 0.01);
+
+                            }
+
+                            if (!armGoingRight) { //makes arm go left
+
+                                armServo.setPosition(armPos - 0.01);
+
+                            }
+
+
+                            if (((armSensor.blue() - armSensor.red()) > 30) && (( armSensor.blue() - armSensor.green()) >30)){ //if there is more blue than any other color by a certain threshold
+
+                                if (armServo.getPosition() <.72){ //if the blue is left of the middle, the robot drives slightly to the right
+
+                                    leftPower = 0.15;
+                                    rightPower = 0.1;
+
+                                }
+
+                                else if (armServo.getPosition() >=.72) { //if the blue is to the right of the middle, the robot drives slightly to the left
+
+                                    leftPower = 0.1;
+                                    rightPower = 0.15;
+
+                                }
+
+                                this.leftMotor.setPower(leftPower); //makes the motors actually move
+                                this.rightMotor.setPower(rightPower);
+
+                            } // if (((armSensor.blue() - armSensor.red()) > 30) && (( armSensor.blue() - armSensor.green()) >30))
+
+                        }//while (followingLine == true)
+                   }// else if (followingLine == true)
+
+                        else { //if the bot is not controlled, and line follow is off, the robot stops moving
+
+                            leftPower = 0;
+                            rightPower = 0;
+
+                        }
+
+
+                       this.leftMotor.setPower(leftPower);
+                       this.rightMotor.setPower(rightPower);
+
+                       telemetry.addData("Left Power", leftPower);
+                       telemetry.addData("Right Power", rightPower);
+                       telemetry.addData("Arm Position", this.armServo.getPosition());
+                       telemetry.addData("ARGB", armSensor.argb());
+                       telemetry.addData("Line Following", followingLine);
+
+                       telemetry.update();
+
+        }//while (opModeIsActive())
     } //public void runOpMode()
 } //public class followLine1 extends LinearOpMode

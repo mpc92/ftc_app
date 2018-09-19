dc9af312c2fcdc1aac01196a0617042da80a870d
varun <mastervarun20@gmail.com>
2018-09-18 19:44:53 -0700

    testing

---
+++ TeamCode/src/main/java/teamcode/IntakeTest.java
@@ -0,0 +1,49 @@
+package teamcode;
+
+
+import com.qualcomm.robotcore.eventloop.opmode.LinearOpMode;
+import com.qualcomm.robotcore.eventloop.opmode.OpMode;
+import com.qualcomm.robotcore.eventloop.opmode.TeleOp;
+
+import com.qualcomm.robotcore.hardware.DcMotor;
+
+import com.qualcomm.robotcore.hardware.DcMotorSimple;
+import com.qualcomm.robotcore.util.ElapsedTime;
+
+import teamcode.libraries.DriveLibrary;
+import teamcode.libraries.IntakeLibrary;
+
+
+@TeleOp(name = "Intake Test", group = "Linear Opmode")
+
+public class IntakeTest extends LinearOpMode{
+
+    private ElapsedTime runtime = new ElapsedTime();
+
+    private DcMotor leftMotor;
+    private DcMotor rightMotor;
+
+    private IntakeLibrary IntakeLibrary = new IntakeLibrary(leftMotor, rightMotor);
+
+    @Override
+    public void runOpMode(){
+
+        waitForStart();
+        runtime.reset();
+
+        this.leftMotor = hardwareMap.get(DcMotor.class, "Left Motor");
+        this.rightMotor = hardwareMap.get(DcMotor.class, "Right Motor");
+        this.rightMotor.setDirection(DcMotorSimple.Direction.REVERSE);
+        telemetry.addData("lspeed", this.leftMotor.getPower());
+        telemetry.addData("rSpeed", this.rightMotor.getPower());
+
+        while (opModeIsActive()){
+
+            this.leftMotor.setPower(gamepad1.right_trigger);
+            this.rightMotor.setPower(gamepad1.right_trigger);
+            telemetry.update();
+
+            }
+        }
+    }
+
---
+++ TeamCode/src/main/java/teamcode/libraries/IntakeLibrary.java
@@ -0,0 +1,40 @@
+package teamcode.libraries;
+
+
+
+import com.qualcomm.robotcore.hardware.DcMotor;
+import com.qualcomm.robotcore.hardware.DcMotorSimple;
+
+public class IntakeLibrary {
+
+    private DcMotor leftMotor;
+    private DcMotor rightMotor;
+
+
+
+    public IntakeLibrary(DcMotor leftMotor, DcMotor rightMotor){
+
+        this.leftMotor = leftMotor;
+        this.rightMotor = rightMotor;
+
+    }
+
+    public void quarter(){
+
+    }
+
+    public void half(){
+
+
+
+    }
+
+    public void three(){
+
+    }
+
+    public void one(){
+
+
+    }
+}

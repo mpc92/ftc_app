34a12276649a283068281ffb2dde07ce652cc694
NateTheM8 <nbalzotti@gmail.com>
2018-09-25 19:47:54 -0700

    Nathans armcontrol changes
    changeschanges changes

--- TeamCode/src/main/java/teamcode/ArmControlTest.java
+++ TeamCode/src/main/java/teamcode/ArmControlTest.java
@@ -11,7 +11,47 @@ public class ArmControlTest extends LinearOpMode {
     private DcMotor armBaseMotor;
     @Override
     //runop calls other methods to control arm- main method
+
+
     public void runOpMode() throws InterruptedException {
+        initialize();
+        while(opModeIsActive()){
+
+        }
+    }
+    public void initialize() {
+        armbasemotor = hardwareMap.get(DcMotor.class, "motor1");
+        armServo = hardwareMap.get(Servo.class, "armServo");
+        motor1.setDirection(DcMotor.Direction.FORWARD);
+        armServoBase.setDirection(Servo.Direction.FORWARD);
+        armServoTop.setDirection(Servo.Direction.FORWARD);
+        clawServo.setDirection(Servo.Direction.FORWARD);
+
+
+
+
+
 
     }
 }
+
+/*
+public class LinearSlideTest extends LinearOpMode {
+    private DcMotor motor1;
+
+    public void runOpMode() {
+        initialize();
+
+        //Always True
+        while (opModeIsActive() ) {
+            float joystickY = gamepad1.right_stick_y;
+            motor1.setPower(joystickY);
+        }
+    }
+
+    public void initialize() {
+        motor1 = hardwareMap.get(DcMotor.class, "motor1");
+        motor1.setDirection(DcMotor.Direction.FORWARD);
+    }
+}
+*/

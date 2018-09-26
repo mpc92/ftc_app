6c9c5e9212f783a59ce18800cb157ea95f345404
NateTheM8 <nbalzotti@gmail.com>
2018-09-25 19:14:32 -0700

    Beginning of arm code
    o ya

---
+++ TeamCode/src/main/java/teamcode/ArmControlTest.java
@@ -0,0 +1,17 @@
+package teamcode;
+
+import com.qualcomm.robotcore.eventloop.opmode.LinearOpMode;
+import com.qualcomm.robotcore.hardware.DcMotor;
+import com.qualcomm.robotcore.hardware.Servo;
+
+public class ArmControlTest extends LinearOpMode {
+    private Servo armServoBase;
+    private Servo armServoTop;
+    private Servo clawServo;
+    private DcMotor armBaseMotor;
+    @Override
+    //runop calls other methods to control arm- main method
+    public void runOpMode() throws InterruptedException {
+
+    }
+}
